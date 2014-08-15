830dfc6583c82da1b772ec26841316316bec34d2
sam <none@none>
2014-08-15 00:22:13 -0400

    add LICENSE.txt, add license notice to AbsurdEngine code

--- src/com/arcadeoftheabsurd/absurdengine/Timer.java
+++ src/com/arcadeoftheabsurd/absurdengine/Timer.java
@@ -1,3 +1,14 @@
+/*
+ * AbsurdEngine (https://bitbucket.org/smpsnr/absurdengine/) 
+ * (c) by Sam Posner (http://www.arcadeoftheabsurd.com/)
+ *
+ * AbsurdEngine is licensed under a
+ * Creative Commons Attribution 4.0 International License
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see http://creativecommons.org/licenses/by/4.0/ 
+ */
+
 package com.arcadeoftheabsurd.absurdengine;
 
 import java.util.Observable;
