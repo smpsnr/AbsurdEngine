0d36aa77ebef16eab84a397a30877491f350c00f
sam <none@none>
2014-08-14 20:24:16 -0400

    add BannerAdView that resizes description text correctly, make NativeAdView extend LinearLayout

---
+++ src/com/arcadeoftheabsurd/absurdengine/BannerAdView.java
@@ -0,0 +1,84 @@
+package com.arcadeoftheabsurd.absurdengine;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.widget.TextView;
+
+import com.mobfox.adsdk.nativeads.NativeAdView;
+
+/**
+ * View that displays a NativeAd as a horizontal banner: [ad icon] [textMarginLeft] [ad description] [textMarginRight]
+ * @author sam
+ */
+
+public class BannerAdView extends NativeAdView
+{
+	private Sprite sprite;
+	private TextView spacerView;
+	private TextView descriptionView;
+	
+	private int textSize;
+	private int textMarginLeft;
+	private int textMarginRight;
+		
+	private boolean readyToWrite = false;
+	private int descriptionWidth;
+	private char[] descriptionChars;
+	private StringBuilder descriptionBuffer;
+	private int curChar;
+	
+	public BannerAdView(Context context, int textSize, int textMarginLeft, int textMarginRight) {
+		super(context);
+		this.textSize = textSize;
+		this.textMarginLeft = textMarginLeft;
+		this.textMarginRight = textMarginRight;
+		
+		setOrientation(HORIZONTAL);
+		spacerView = new TextView(context);
+		descriptionView = new TextView(context);
+	}
+	
+	protected void setAssets(Sprite sprite, String description) {
+		this.sprite = sprite;
+		this.sprite.setLocation(0, 0);
+		descriptionChars = description.toCharArray();
+		
+		descriptionWidth = (this.getWidth() - sprite.getWidth()) - (textMarginLeft + textMarginRight);
+		descriptionBuffer = new StringBuilder();
+		curChar = 0;
+		
+		descriptionBuffer.append(descriptionChars[curChar++]);
+		
+		descriptionView.setText(descriptionBuffer.toString());
+		descriptionView.setTextColor(Color.WHITE);
+		descriptionView.setTextSize(textSize);
+		
+		readyToWrite = true;
+		
+		addView(spacerView, new LayoutParams(sprite.getWidth() + textMarginLeft, sprite.getHeight()));
+		addView(descriptionView, new LayoutParams(descriptionWidth, sprite.getHeight()));		
+	}
+	
+	@Override
+	protected void dispatchDraw(Canvas canvas) {
+		if (sprite != null) {
+			sprite.draw(canvas);
+		}		
+		super.dispatchDraw(canvas);
+		
+		if (readyToWrite) {
+			if (curChar < descriptionChars.length) {
+				descriptionBuffer.append(descriptionChars[curChar++]);
+				
+				if (descriptionView.getWidth() > descriptionWidth) {
+					descriptionBuffer.insert(curChar, " ");
+				}
+				descriptionView.setText(descriptionBuffer.toString());
+				dispatchDraw(canvas);
+			} else {
+				readyToWrite = false;
+			}
+		}
+	}
+}
--- src/com/mobfox/adsdk/nativeads/NativeAdView.java
+++ src/com/mobfox/adsdk/nativeads/NativeAdView.java
@@ -5,69 +5,30 @@ import java.util.List;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
-import com.arcadeoftheabsurd.absurdengine.Sprite;
 import com.arcadeoftheabsurd.absurdengine.WebUtils;
 import com.mobfox.adsdk.nativeads.NativeAd.Tracker;
 
-public class NativeAdView extends FrameLayout
+// this should really be a FrameView that hosts any View type,
+// but on iOS XMLVM does not lay out the contents of FrameViews correctly
+public class NativeAdView extends LinearLayout implements NativeAdListener
 {
-	private Context context;
-	private NativeBanner bannerView;
 	private boolean impressionReported = false;
 	private List<Tracker> trackers;
 
-	class NativeBanner extends RelativeLayout 
-	{
-		Sprite sprite;
-		TextView descriptionView;
-		LayoutParams descriptionLayout; 
-		
-		public NativeBanner(Context context, Sprite sprite, String description) {
-			super(context);
-			this.sprite = sprite;
-			
-			descriptionView = new TextView(context);
-			descriptionView.setText(description);
-			descriptionView.setTextColor(Color.WHITE);
-			descriptionLayout = new LayoutParams(NativeAdView.this.getWidth() - sprite.getWidth(), NativeAdView.this.getHeight());
-			descriptionLayout.addRule(CENTER_VERTICAL);
-			descriptionLayout.addRule(ALIGN_PARENT_RIGHT);
-			
-			addView(descriptionView, descriptionLayout);
-		}
-		
-		@Override
-		protected void dispatchDraw(Canvas canvas) {
-			super.dispatchDraw(canvas);
-			sprite.setLocation(0, -20);
-			sprite.draw(canvas);
-						
-			System.out.println("banner width: " + getWidth() + " banner height: " + getHeight());
-			System.out.println("sprite width: " + sprite.getWidth() + " sprite height: " + sprite.getHeight());
-		}
-	}
-
 	public NativeAdView(Context context) {
 		super(context);
-		this.context = context;
 	}
-	
-	@SuppressWarnings("deprecation")
-	public void setAd(NativeAd ad) {
-		if (bannerView != null) {
-			this.removeView(bannerView);
-		}
+		
+	@Override
+	public void adLoaded(NativeAd ad) {
 		trackers = ad.getTrackers();
-		bannerView = new NativeBanner(context, ad.getImageAsset("icon").sprite, ad.getTextAsset("description"));
-		addView(bannerView, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
-	
 		invalidate();
 	}
+
+	@Override
+	public void adFailedToLoad() {}
 
 	@Override
 	protected void dispatchDraw(Canvas canvas) {
@@ -75,16 +36,21 @@ public class NativeAdView extends FrameLayout
 			if (!impressionReported) {
 				impressionReported = true;
 								
-				for (Tracker t : trackers) {
-					if (t.type.equals("impression")) {
-						try {
-							System.out.println("tracking impression...");
-							System.out.println(WebUtils.restRequest(t.url));
-						} catch (IOException e) {
-							System.out.println("impression failed");
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						for (Tracker t : trackers) {
+							if (t.type.equals("impression")) {
+								try {
+									System.out.println("tracking impression...");
+									System.out.println(WebUtils.restRequest(t.url));
+								} catch (IOException e) {
+									System.out.println("impression failed");
+								}
+							}
 						}
-					}
-				}
+					}	
+				}).start();
 			}
 		}
 		super.dispatchDraw(canvas);
