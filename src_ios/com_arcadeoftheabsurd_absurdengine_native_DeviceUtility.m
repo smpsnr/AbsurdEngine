75fe3c69c13be1fea4160bf4075df4f953ddadd1
sam <none@none>
2014-08-17 19:43:38 -0400

    add license notice to AbsurdEngine C code

--- src_ios/com_arcadeoftheabsurd_absurdengine_native_DeviceUtility.m
+++ src_ios/com_arcadeoftheabsurd_absurdengine_native_DeviceUtility.m
@@ -1,3 +1,13 @@
+/*
+ * AbsurdEngine (https://bitbucket.org/smpsnr/absurdengine/) 
+ * (c) by Sam Posner (http://www.arcadeoftheabsurd.com/)
+ *
+ * AbsurdEngine is licensed under a
+ * Creative Commons Attribution 4.0 International License
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see http://creativecommons.org/licenses/by/4.0/ 
+ */
 
 #include "xmlvm.h"
 #include "com_arcadeoftheabsurd_absurdengine_DeviceUtility.h"
